8d3004893d60b6cd3dc87be30f5f5eeb97096cb3
anujsinghrawat <2003013019@ipec.org.in>
2022-08-13 11:33:51 +0530

    intial
    Signed-off-by: anujsinghrawat <2003013019@ipec.org.in>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Tempapp from "./components/Tempapp";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("almora");
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a6cbb38c0b717d146e22394a96a1f3f6`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      setCity(resJson.main);
+      console.log(resJson);
+      console.log(city.temp);
+    };
+
+    fetchApi();
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Tempapp/>
+    <div className="box">
+      <div className="inputData">
+        <input
+          className="inputField"
+          type="search"
+          value={search}
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
+      </div>
+      {!city? (
+        <p className="errorMsg">No data found</p>
+      ) : (
+        <div>
+          <div className="info">
+            <h2 className="location">
+              <i className="fa-solid fa-location-pin"></i> {search}
+            </h2>
+            {/* <h1 className="temp">50</h1> */}
+            <h1 className="temp">{city.temp}</h1>
+
+            <h3 className="tempmin_max">Min {city.temp_min} : max {city.temp_max}</h3>
+          </div>
+          <div className="wave -one"></div>
+          <div className="wave -two"></div>
+          <div className="wave -three"></div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Tempapp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./css/style.css";
+
+const Tempapp = () => {
+    <>
+        <div className='box'>
+            <div className='inputData'>
+                <input
+                    className="inputField"
+                    type="search"
+                />
+                
+
+            </div>
+
+        </div>
+    
+    </>
+    
+}
+
+export default Tempapp;
